b75fe53c3a222766a623c967ab782d20b6b11f42
Pavire18 <pavire182000@gmail.com>
2024-06-20 17:57:35 +0200

    corrección CORS

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,13 +2,18 @@ import express from "express";
 import cors from "cors";
 import { configureRoutes } from "../routes";
 
+import dotenv from "dotenv";
+dotenv.config();
+
+const FRONT_END_URL: string = process.env.FRONT_END_URL as string;
+
 // Configuración del server
 export const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: [FRONT_END_URL, "http://localhost:4000"],
   })
 );
 
