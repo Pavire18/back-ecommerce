c726d91136c04dcf784af55ba94f3ba313cc60a6
francesc <you@example.com>
2024-06-26 20:20:02 +0200

    added new seed product and category

--- src/domain/seeds/category.data.ts
+++ src/domain/seeds/category.data.ts
@@ -6,15 +6,15 @@ export const categoryWomen = new Category({ name: { es: "Mujer", en: "Women" }, 
 
 export const categories: ICategory[] = [
   { name: { es: "Casual", en: "Casual" }, parentCategory: categoryMen.id },
-  { name: { es: "Deporte", en: "Sports" }, parentCategory: categoryMen.id },
+  { name: { es: "Mocasines", en: "Loafers" }, parentCategory: categoryMen.id },
   { name: { es: "Formal", en: "Formal" }, parentCategory: categoryMen.id },
   { name: { es: "Exterior", en: "Outdoor" }, parentCategory: categoryMen.id },
-  { name: { es: "Casual", en: "Casual" }, parentCategory: categoryKids.id },
-  { name: { es: "Deporte", en: "Sports" }, parentCategory: categoryKids.id },
-  { name: { es: "Formal", en: "Formal" }, parentCategory: categoryKids.id },
-  { name: { es: "Exterior", en: "Outdoor" }, parentCategory: categoryKids.id },
-  { name: { es: "Casual", en: "Casual" }, parentCategory: categoryWomen.id },
-  { name: { es: "Deporte", en: "Sports" }, parentCategory: categoryWomen.id },
-  { name: { es: "Formal", en: "Formal" }, parentCategory: categoryWomen.id },
-  { name: { es: "Exterior", en: "Outdoor" }, parentCategory: categoryWomen.id }
+  { name: { es: "Náuticos", en: "Boat Shoes" }, parentCategory: categoryKids.id },
+  { name: { es: "Deportes", en: "Sports" }, parentCategory: categoryKids.id },
+  { name: { es: "Velcro", en: "Velcro" }, parentCategory: categoryKids.id },
+  { name: { es: "Sandalias", en: "Sandals" }, parentCategory: categoryKids.id },
+  { name: { es: "Tacon", en: "Heels" }, parentCategory: categoryWomen.id },
+  { name: { es: "Alpargatas", en: "Espadrilles" }, parentCategory: categoryWomen.id },
+  { name: { es: "Para vestir", en: "Dress Shoes" }, parentCategory: categoryWomen.id },
+  { name: { es: "Zapatillas", en: "Sneakers" }, parentCategory: categoryWomen.id }
 ]
